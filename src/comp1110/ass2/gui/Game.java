eb794a2c90f061d6192e123ec6288cd9d219fe16
Akshat Singhal <u6322388@anu.edu.au>
2018-04-18 07:23:58 +1000

    Putting the controls in T9

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,6 +1,10 @@
 package comp1110.ass2.gui;
 
+import comp1110.ass2.WarringStatesGame;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -8,6 +12,21 @@ public class Game extends Application {
     private static final int BOARD_HEIGHT = 700;
 
     // FIXME Task 9: Implement a basic playable Warring States game in JavaFX
+    private static final int VIEWER_WIDTH = 933;
+    private static final int VIEWER_HEIGHT = 700;
+    private static final String URI_BASE="assets/";
+
+    private WarringStatesGame warringStatesGame;
+    private final Group moveright= new Group();
+    private final Group moveleft= new Group();
+
+    private final Group root = new Group();
+    private final Group controls = new Group();
+    private final Group squareg = new Group();
+    private final Group pieces = new Group();
+
+    private final String [][]vmove={};
+
 
     // FIXME Task 11: Allow players of your Warring States game to play against your simple agent
 
@@ -15,6 +34,18 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Warring States Game");
+        Scene scene= new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
+
+        root.getChildren().add(controls);
+        root.getChildren().add(moveleft);
+        root.getChildren().add(moveright);
+        root.getChildren().add(squareg);
+        root.getChildren().add(pieces);
+
+
+
 
     }
 }
